45dfbda78fbadd9ab46c4d08fbb83ef349af36f1
Yurtsiv Stepan <yurtsiv.stepan@gmail.com>
2019-03-24 18:59:05 +0100

    optimized bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,14 @@
-def is_sorted(array):
-  min = array[0]
-  for elem in array:
-    if elem < min:
-      return False
-  return True
-
 def bubble_sort(array):
   result = array[:]
-  while not is_sorted(result):
+  swapped = True
+
+  while swapped:
+    swapped = False
     for index in range(0, len(result) - 1):
       if result[index] > result[index+1]:
         temp = result[index]
         result[index] = result[index+1]
         result[index+1] = temp
+        swapped = True
+
   return result
